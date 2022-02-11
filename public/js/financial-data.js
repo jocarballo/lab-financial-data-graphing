283c8d2cdeabd3f63f1e9b3a885ced41cfe0fa78
Joana Carballo <joana16cruz@gmail.com>
2022-02-11 16:36:50 +0100

    iteration 2

--- public/js/financial-data.js
+++ public/js/financial-data.js
@@ -1,10 +1,9 @@
 
-const getData = input => {
-
     //http://api.coindesk.com/v1/bpi/historical/close.json
     // make a request to the API
     axios.get('http://api.coindesk.com/v1/bpi/historical/close.json')
         .then(response => {
+            printChart(response.data)
             console.log(response.data)
         })
         .catch(err => {
@@ -13,6 +12,41 @@ const getData = input => {
         })
 
     
+
+const printChart = data => {
+    //console.log(data)
+    const dailyData = data.bpi
+    console.log(dailyData)
+    // object with dates (keys)
+    const stockDates = Object.keys(dailyData)
+    console.log(stockDates)
+    const stockPrices = stockDates.map(date => {
+        return dailyData[date]
+    })
+    console.log('stock prices: ', stockPrices)
+
 }
 
-getData();
+
+//drawing the chart
+//canvas Object
+let ctx = document.querySelector('#myChart').getContext('2d')
+
+
+new Chart(ctx, {
+    type: 'line', 
+    data: {
+        // x - axis
+        labels: stockDates,
+        datasets: [
+            {
+                label: 'Stock Chart',
+                backGroundColor: 'rgb(255, 99, 132)',
+				borderColor: 'rgb(255, 99, 132)',
+                // y - axis
+                data: stockPrices
+            }
+        ]
+    }
+})
+
--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,6 @@
 const router = require("express").Router();
-const axios = require('axios')
-
 /* GET home page */
 router.get("/", (req, res, next) => {
-  axios.get('http://api.coindesk.com/v1/bpi/historical/close.json')
-    .then(response => {
-      const dataFromAPI = response.data
-      console.log(response.data)
-      res.render('index');
-    })
-    .catch(err => next(err))
- 
+  res.render("index");
 });
-
 module.exports = router;
